523e0923896eef8f10bd19d5f9728eb64850612f
Xannor <2722473+xannor@users.noreply.github.com>
2022-09-15 00:35:28 +0000

    fixed dropped dep reference and code cleanup

--- src/async_reolink/api/const.py
+++ src/async_reolink/api/const.py
@@ -1,35 +1,7 @@
 """ Constants """
 
-from enum import IntEnum
-from typing import Final, Literal
-from backports.strenum import StrEnum
+from typing import Final
 
 DEFAULT_TIMEOUT: Final = 30
 DEFAULT_USERNAME: Final = "admin"
 DEFAULT_PASSWORD: Final = ""
-
-STREAM_TYPES: Final = Literal["main", "sub", "ext"]
-
-
-class IntStreamTypes(IntEnum):
-    """Stream Types"""
-
-    MAIN = 0
-    SUB = 1
-    EXT = 2
-
-
-class StreamTypes(StrEnum):
-    """Stream Types"""
-
-    MAIN = "main"
-    SUB = "sub"
-    EXT = "ext"
-
-
-class LightTypes(IntEnum):
-    """Light Types"""
-
-    IR = 0
-    POWER = 1
-    WHITE = 2
